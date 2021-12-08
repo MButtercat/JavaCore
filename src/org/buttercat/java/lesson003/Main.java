ab62451c7c1574880a041fbd6fc98a9f534bd22b
Semyonov <semyonov@mail.lol>
2021-12-08 16:57:02 +0300

    Add third lesson

--- src/org/buttercat/java/lesson003/Main.java
+++ src/org/buttercat/java/lesson003/Main.java
@@ -1,7 +1,9 @@
 package org.buttercat.java.lesson003;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 class listHelper {
     private ArrayList<Integer> l3, l5, l35;
@@ -34,11 +36,15 @@ class listHelper {
 
 public class Main {
     final static int oddMax = 99;
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
         lesson3 l3 = new lesson3();
-// Task 1
+
+        // Task 1
+        System.out.println(" ------ Task 1 ------ ");
+
         try {
-            System.out.print("list of first " + oddMax + " numbers: ");
+            System.out.print("list of first odd numbers up to " + oddMax + ": ");
             for (Integer i : l3.oddGen(oddMax)) {
                 System.out.print(i + (i < oddMax-1 ? ", " : "\n"));
             }
@@ -46,7 +52,10 @@ public class Main {
         catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+
 // Task 2
+        System.out.println("\n ------ Task 2 ------ ");
+
         try {
             listHelper lm = l3.commonMultiples(oddMax+1);
             System.out.print("List of numbers with multiple 3: ");
@@ -65,5 +74,58 @@ public class Main {
         catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
+
+// Task 3
+        System.out.println("\n ------ Task 3 ------ ");
+
+        BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
+        Integer[] arr = new Integer[3];
+        String[] ind = new String[]{"first", "second", "third"};
+        for(int j = 0; j < 3; j++) {
+            System.out.print("Enter the " + ind[j] + " number: ");
+            arr[j] = Integer.parseInt(inp.readLine());
+        }
+        System.out.println("Result: " + l3.isSumCorrect(arr));
+
+// Task 4
+        System.out.println("\n ------ Task 4 ------ ");
+
+        for(int j = 0; j < 3; j++) {
+            System.out.print("Enter the " + ind[j] + " number: ");
+            arr[j] = Integer.parseInt(inp.readLine());
+        }
+        System.out.println("Result: " + l3.isCompCorrect(arr));
+
+// Task 5
+        System.out.println("\n ------ Task 5 ------ ");
+        boolean res = false;
+        int attempts = 1;
+        do {
+            int arrSz = ThreadLocalRandom.current().nextInt(5, 16);
+            System.out.println("Attempt " + attempts + " for finding perfect array with " + arrSz + " elements");
+            int[] rndArr = new int[arrSz];
+            for (int i = 0; i < arrSz; i++) {
+                rndArr[i] = ThreadLocalRandom.current().nextInt(-5, 6);
+            }
+            System.out.println("Our array: " + Arrays.toString(rndArr));
+            System.out.println("First element: " + rndArr[0] + (rndArr[0] == 3 ? " is equal 3" : " isn't equal 3"));
+            System.out.println("Last element: " + rndArr[rndArr.length-1] + (rndArr[rndArr.length-1] == 3 ? " is equal 3" : " isn't equal 3"));
+            res =((rndArr[0] == 3) | (rndArr[rndArr.length-1] == 3));
+            attempts++;
+            System.out.println("");
+        } while(!res);
+        System.out.println("Hooray! We find it!");
+
+// Task 6
+        System.out.println("\n ------ Task 6 ------ ");
+
+        int arrSz = ThreadLocalRandom.current().nextInt(10, 31);
+        System.out.println("Generate array with " + arrSz + " elements");
+        int[] rndArr = new int[arrSz];
+        for (int i = 0; i < arrSz; i++) {
+            rndArr[i] = ThreadLocalRandom.current().nextInt(-5, 6);
+        }
+        System.out.println("Our array: " + Arrays.toString(rndArr));
+        System.out.println("This array " + (IntStream.of(rndArr).anyMatch(x -> x == 3) ? "contains 3" : "doesn't contain 3"));
     }
 }
--- src/org/buttercat/java/lesson003/lesson3.java
+++ src/org/buttercat/java/lesson003/lesson3.java
@@ -12,7 +12,7 @@ public class lesson3 {
         }
         return l;
     }
-    public listHelper commonMultiples(int max) throws IllegalArgumentException  {
+    public listHelper commonMultiples(int max) throws IllegalArgumentException {
         if(max <= 0)
             throw new IllegalArgumentException("Invalid argument");
         listHelper l = new listHelper();
@@ -31,5 +31,16 @@ public class lesson3 {
 
         }
         return l;
+    }
+    public boolean isSumCorrect(Integer[] i) throws IllegalArgumentException {
+        if(i.length != 3)
+            throw new IllegalArgumentException("Invalid argument");
+        return i[0]+i[1] == i[2];
+    }
+
+    public boolean isCompCorrect(Integer[] i) throws IllegalArgumentException {
+        if(i.length != 3)
+            throw new IllegalArgumentException("Invalid argument");
+        return ((i[0].intValue() < i[1].intValue()) & (i[1].intValue() < i[2].intValue()));
     }
 }
